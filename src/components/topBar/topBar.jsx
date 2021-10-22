9e98bb52d5913de1875030be6b5314751b40b143
luckyhyom <bs_khm@naver.com>
2021-10-22 19:00:33 +0900

    Top bar UI 작업, PostCSS

--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -1,8 +1,10 @@
-import { Button } from "@mui/material"
+import { Button, AppBar, Box, Toolbar, Typography } from "@mui/material"
+import SnowshoeingIcon from '@mui/icons-material/Snowshoeing';
 import { useState } from "react";
 import { useHistory } from "react-router";
 import { Login } from "../login/login";
 import { ProfileUpdateForm } from "../profileUpdateForm/profileUpdateForm";
+import styles from './topBar.module.css'
 
 export const TopBar = ({ user, onLogin, authService, errorMessage, onLogout, updateProfile, changeProfileImage }) => {
     const history =  useHistory();
@@ -10,31 +12,39 @@ export const TopBar = ({ user, onLogin, authService, errorMessage, onLogout, upd
     const [loginFormModal, setLoginFormModal] = useState(false);
 
     return (
-        <nav>
+        <Box>
+            <AppBar>
+                <Toolbar className={styles.toolbar}>
+                    <SnowshoeingIcon/>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Baeum
+                    </Typography>
 
-            { user && <ul>
-                <Button onClick={ onLogout }>Logout</Button>
-                <Button onClick={ () => setMyInfoModal(boolean=> !boolean) }>My Info</Button>
-                <div style={{ display: myInfoModal ? 'block':'none' }} >
-                    <img src={ user.profileImageURL } style={{ width: '300px', height: '300px', objectFit: 'cover' }}/>
-                    <input type="file" onChange={ (event) => changeProfileImage(event) }/>
-                    <ProfileUpdateForm 
-                        updateProfile={ updateProfile }
-                        about={ user.about }/>
-                </div>
-            </ul> }
+                    { user && <ul>
+                        <Button className={styles.button} color="inherit" onClick={ onLogout } variant="outlined">Logout</Button>
+                        <Button  color="inherit"  onClick={ () => setMyInfoModal(boolean=> !boolean) }>My Info</Button>
+                        <div style={{ display: myInfoModal ? 'block':'none' }} >
+                            <img src={ user.profileImageURL } style={{ width: '300px', height: '300px', objectFit: 'cover' }}/>
+                            <input type="file" onChange={ (event) => changeProfileImage(event) }/>
+                            <ProfileUpdateForm 
+                                updateProfile={ updateProfile }
+                                about={ user.about }/>
+                        </div>
+                    </ul> }
 
-            { !user && <ul>
-                <Button onClick={ () => history.push('/signup') } >Sign Up</Button>
-                <Button onClick={ () => setLoginFormModal(boolean => !boolean) } >Sign In</Button>
-                <div style={{display: loginFormModal ? 'block':'none'}}>
-                    <Login
-                        onLogin={ onLogin }
-                        authService={ authService }
-                        errorMessage={ errorMessage }
-                    />
-                </div>
-            </ul> }
-        </nav>
+                    { !user && <ul>
+                        <Button className={styles.button} color="inherit" onClick={ () => history.push('/signup') } variant="outlined" >Sign Up</Button>
+                        <Button  color="inherit" onClick={ () => setLoginFormModal(boolean => !boolean) } variant="outlined" >Sign In</Button>
+                        <div style={{display: loginFormModal ? 'block':'none'}}>
+                            <Login
+                                onLogin={ onLogin }
+                                authService={ authService }
+                                errorMessage={ errorMessage }
+                            />
+                        </div>
+                    </ul> }
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
