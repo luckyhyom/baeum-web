24574f25b75b61e386250730c4546577cd88e48b
luckyhyom <bs_khm@naver.com>
2021-10-22 19:34:40 +0900

    로그인 모달창 작업

--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -1,4 +1,4 @@
-import { Button, AppBar, Box, Toolbar, Typography } from "@mui/material"
+import { Button, AppBar, Box, Toolbar, Typography, Modal } from "@mui/material"
 import SnowshoeingIcon from '@mui/icons-material/Snowshoeing';
 import { useState } from "react";
 import { useHistory } from "react-router";
@@ -6,10 +6,26 @@ import { Login } from "../login/login";
 import { ProfileUpdateForm } from "../profileUpdateForm/profileUpdateForm";
 import styles from './topBar.module.css'
 
+const signInStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};  
+
 export const TopBar = ({ user, onLogin, authService, errorMessage, onLogout, updateProfile, changeProfileImage }) => {
     const history =  useHistory();
     const [myInfoModal, setMyInfoModal] = useState(false);
-    const [loginFormModal, setLoginFormModal] = useState(false);
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false); 
+
 
     return (
         <Box>
@@ -22,7 +38,7 @@ export const TopBar = ({ user, onLogin, authService, errorMessage, onLogout, upd
 
                     { user && <ul>
                         <Button className={styles.button} color="inherit" onClick={ onLogout } variant="outlined">Logout</Button>
-                        <Button  color="inherit"  onClick={ () => setMyInfoModal(boolean=> !boolean) }>My Info</Button>
+                        <Button  color="inherit"  onClick={ () => setMyInfoModal(boolean=> !boolean) } variant="outlined" >My Info</Button>
                         <div style={{ display: myInfoModal ? 'block':'none' }} >
                             <img src={ user.profileImageURL } style={{ width: '300px', height: '300px', objectFit: 'cover' }}/>
                             <input type="file" onChange={ (event) => changeProfileImage(event) }/>
@@ -34,14 +50,21 @@ export const TopBar = ({ user, onLogin, authService, errorMessage, onLogout, upd
 
                     { !user && <ul>
                         <Button className={styles.button} color="inherit" onClick={ () => history.push('/signup') } variant="outlined" >Sign Up</Button>
-                        <Button  color="inherit" onClick={ () => setLoginFormModal(boolean => !boolean) } variant="outlined" >Sign In</Button>
-                        <div style={{display: loginFormModal ? 'block':'none'}}>
+                        <Button  color="inherit" onClick={ handleOpen } variant="outlined" >Sign In</Button>
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={signInStyle}>
                             <Login
                                 onLogin={ onLogin }
                                 authService={ authService }
                                 errorMessage={ errorMessage }
                             />
-                        </div>
+                            </Box>
+                        </Modal>
                     </ul> }
                 </Toolbar>
             </AppBar>
