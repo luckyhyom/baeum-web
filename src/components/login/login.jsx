c0c2ff3e4639608093dd4f0cd0d180dd0cd85c98
luckyhyom <bs_khm@naver.com>
2021-10-24 17:18:48 +0900

    로그인 모달창 모바일 대응

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {Button, TextField} from "@mui/material"
+import { Box, Button, TextField, Typography } from "@mui/material"
 
 export const Login = ({ onLogin, errorMessage }) => {
     const [body, setBody] = useState({ userId: '', password: '' })
@@ -18,11 +18,22 @@ export const Login = ({ onLogin, errorMessage }) => {
 
     return (
         <>
-            <form onSubmit={ onSubmit }>
-                <TextField id="outlined-basic" label="ID" variant="outlined" name="userId" onChange={onChange} fullWidth margin="normal" />
-                <TextField id="outlined-basic" label="Password" variant="outlined"  type="password" name="password" onChange={onChange} fullWidth margin="normal" />
-                <Button type="submit" variant="contained" fullWidth>submit</Button>
-            </form>
+            <Box component="form"
+                onSubmit={ onSubmit }
+                method="POST"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h6">
+                    Sign in
+                </Typography>
+                <TextField label="ID" variant="outlined" name="userId" onChange={onChange} fullWidth margin="normal" />
+                <TextField label="Password" variant="outlined"  type="password" name="password" onChange={onChange} fullWidth margin="normal" />
+                <Button type="submit" variant="contained" sx={{ mt: 3 }} fullWidth>Sign in</Button>
+            </Box>
             { errorMessage && <p style={{ color: 'red' }}>{ errorMessage }</p> }
         </>
     )
--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -10,9 +10,9 @@ const signInStyle = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: '80%',
+    maxWidth: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
     boxShadow: 24,
     p: 4,
 };  
