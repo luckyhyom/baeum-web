c96e8380a5d09f5fdb6754d94514496ab947dc1f
luckyhyom <bs_khm@naver.com>
2021-10-27 12:40:19 +0900

    api 서버 URL 추가

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import AuthService from './service/auth';
 import FileUploader from './service/fileUploader';
 import LectureService from './service/lecture';
 
-const baseURL = 'http://127.0.0.1:3000';
+// const baseURL = 'http://127.0.0.1:3000';
+const baseURL = 'https://www.makevalue.net/api';
+
 const httpClient = new HttpClient(baseURL, getCsrfToken);
 const authService = new AuthService(httpClient);
 const fileUploader = new FileUploader(httpClient);
