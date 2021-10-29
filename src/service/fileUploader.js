5c3f7cf96442ffd602db5ab71137750e2f74f684
luckyhyom <bs_khm@naver.com>
2021-10-29 15:13:41 +0900

    s3 패키지 변경, 파일명 변경하여 업로드

--- src/service/fileUploader.js
+++ src/service/fileUploader.js
@@ -1,11 +1,26 @@
-import { config } from '../config/config'
-import { uploadFile } from 'react-s3'
+import { config } from '../config/config';
+import S3 from 'react-aws-s3';
 
 const s3Keys = {
     bucketName: config.aws.s3.BUCKET_NAME,
     region: config.aws.s3.REGION,
     accessKeyId: config.aws.s3.ACCESS_KEY,
     secretAccessKey: config.aws.s3.SECRET_KEY,
+}
+export default class FileUploader {
+    constructor(http) {
+        this.http = http
+    }
+
+    uploadImage(file) {
+        return new S3(s3Configs('/profile'))
+            .uploadFile(file, newFileName(file));
+    }
+
+    uploadThumbnail(file) {
+        return new S3(s3Configs('/lectures/thumbnail'))
+            .uploadFile(file, newFileName(file));
+    }
 }
 
 function s3Configs(dirName) {
@@ -17,16 +32,7 @@ function s3Configs(dirName) {
         dirName: `baeumFiles${dirName}`
     }
 }
-export default class FileUploader {
-    constructor(http) {
-        this.http = http
-    }
 
-    uploadImage(file) {
-        return uploadFile(file,s3Configs('/profile'))
-    }
-
-    uploadThumbnail(file) {
-        return uploadFile(file,s3Configs('/lectures/thumbnail'));
-    }
+function newFileName(file) {
+    return new Date().getTime() + file.name;
 }
